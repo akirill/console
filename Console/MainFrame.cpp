akirill/console

commit b8176836447d5097e4b406c9c6dc0c961619d26f

- reintegrated branch b142_new_old_repaint
git-svn-id: svn://localhost/private/Console2/trunk@985 8ae08d04-e83e-804b-908e-3db21359c104

Console/MainFrame.cpp
@@ -684,15 +684,13 @@ LRESULT MainFrame::OnWindowPosChanging(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM 
 
 		if (m_activeView.get() != NULL)
 		{
-			shared_ptr<TabData> tabData = m_activeView->GetTabData();
+			CRect rectClient;
+			GetClientRect(&rectClient);
 
-			// only for relative backgrounds
-			if (tabData->imageData.bRelative)
-			{
-				CRect rectClient;
-				GetClientRect(&rectClient);
-				InvalidateRect(&rectClient, FALSE);
-			}
+			m_activeView->MainframeMoving();
+			// we need to invalidate client rect here for proper background 
+			// repaint when using relative backgrounds
+			InvalidateRect(&rectClient, FALSE);
 		}
 
 		return 0;
@@ -858,7 +856,7 @@ LRESULT MainFrame::OnUpdateTitles(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*
 		{
 			m_strWindowTitle = consoleView->GetTitle();
 			SetWindowText(m_strWindowTitle);
-			SetTrayIcon(NIM_MODIFY);
+			if (g_settingsHandler->GetAppearanceSettings().stylesSettings.bTrayIcon) SetTrayIcon(NIM_MODIFY);
 		}
 	}
 	else
@@ -885,7 +883,7 @@ LRESULT MainFrame::OnUpdateTitles(UINT /*uMsg*/, WPARAM wParam, LPARAM /*lParam*
 			if (windowSettings.bShowCommand)	m_strWindowTitle += strCommandText;
 
 			SetWindowText(m_strWindowTitle);
-			SetTrayIcon(NIM_MODIFY);
+			if (g_settingsHandler->GetAppearanceSettings().stylesSettings.bTrayIcon) SetTrayIcon(NIM_MODIFY);
 		}
 		
 		if (windowSettings.bShowCommandInTabs) strTabTitle += strCommandText;
@@ -1371,7 +1369,7 @@ LRESULT MainFrame::OnEditSettings(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWn
 		m_activeView->InitializeScrollbars();
 		m_activeView->RecreateOffscreenBuffers();
 		AdjustWindowSize(false);
-		m_activeView->RepaintView();
+		m_activeView->Repaint(true);
 	}
 
 	RegisterGlobalHotkeys();

Console/Console.rc
@@ -644,8 +644,8 @@ END
 //
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 2,0,0,140
- PRODUCTVERSION 2,0,0,140
+ FILEVERSION 2,0,0,144
+ PRODUCTVERSION 2,0,0,144
  FILEFLAGSMASK 0x3fL
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -663,13 +663,13 @@ BEGIN
             VALUE "Comments", "Tabbed cool console window (THIS IS NOT A SHELL :-)"
             VALUE "CompanyName", "Ingenuity Unlimited Ltd."
             VALUE "FileDescription", "Main Console module"
-            VALUE "FileVersion", "2.00, Build 140 2008.08.21"
+            VALUE "FileVersion", "2.00, Build 144 2009.03.23"
             VALUE "InternalName", "Console"
-            VALUE "LegalCopyright", "Copyright © 2001-2008 Marko Bozikovic"
-            VALUE "LegalTrademarks", "Copyright © 2001-2008 Marko Bozikovic"
+            VALUE "LegalCopyright", "Copyright © 2001-2009 Marko Bozikovic"
+            VALUE "LegalTrademarks", "Copyright © 2001-2009 Marko Bozikovic"
             VALUE "OriginalFilename", "Console.exe"
             VALUE "ProductName", "Console"
-            VALUE "ProductVersion", "2, 0, 0, 140"
+            VALUE "ProductVersion", "2, 0, 0, 144"
         END
     END
     BLOCK "VarFileInfo"

Console/Console.vcproj
@@ -58,7 +58,7 @@
 				RuntimeLibrary="3"
 				UsePrecompiledHeader="2"
 				WarningLevel="4"
-				Detect64BitPortabilityProblems="true"
+				Detect64BitPortabilityProblems="false"
 				DebugInformationFormat="4"
 			/>
 			<Tool
@@ -147,7 +147,7 @@
 				RuntimeLibrary="2"
 				UsePrecompiledHeader="2"
 				WarningLevel="4"
-				Detect64BitPortabilityProblems="true"
+				Detect64BitPortabilityProblems="false"
 				DebugInformationFormat="0"
 			/>
 			<Tool

Console/ConsoleView.cpp
@@ -36,6 +36,7 @@ ConsoleView::ConsoleView(MainFrame& mainFrame, DWORD dwTabIndex, const wstring& 
 , m_bResizing(false)
 , m_bAppActive(true)
 , m_bActive(true)
+, m_bNeedFullRepaint(true) // first OnPaint will do a full repaint
 , m_bUseTextAlphaBlend(false)
 , m_bConsoleWindowVisible(false)
 , m_dwStartupRows(dwRows)
@@ -258,10 +259,12 @@ LRESULT ConsoleView::OnPaint(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/
 
 	CPaintDC	dc(m_hWnd);
 
-	if ((m_tabData->backgroundImageType != bktypeNone) && m_tabData->imageData.bRelative)
+	if (m_bNeedFullRepaint)
 	{
-		// we need to update offscreen buffers here for relative backgrounds
+		// we need to update offscreen buffers here for first paint and relative backgrounds
+		RepaintText(m_dcText);
 		UpdateOffscreen(dc.m_ps.rcPaint);
+		m_bNeedFullRepaint = false;
 	}
 
 	// this is the best way I know how to detect if the window is being 
@@ -295,7 +298,7 @@ LRESULT ConsoleView::OnWindowPosChanged(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM
 	WINDOWPOS* pWinPos = reinterpret_cast<WINDOWPOS*>(lParam);
 
 	// showing the view, repaint
-	if (pWinPos->flags & SWP_SHOWWINDOW) Repaint();
+	if (pWinPos->flags & SWP_SHOWWINDOW) Repaint(false);
 
 	return 0;
 }
@@ -828,7 +831,7 @@ LRESULT ConsoleView::OnUpdateConsoleView(UINT /*uMsg*/, WPARAM wParam, LPARAM /*
 		m_selectionHandler->UpdateSelection();
 	}
 
-	Repaint();
+	Repaint(false);
 	return 0;
 }
 
@@ -1073,10 +1076,37 @@ void ConsoleView::RecreateOffscreenBuffers()
 
 //////////////////////////////////////////////////////////////////////////////
 
-void ConsoleView::RepaintView()
+void ConsoleView::Repaint(bool bFullRepaint)
 {
-	RepaintText();
+	// OnPaint will do the work for a full repaint
+	if (!m_bNeedFullRepaint)
+	{
+		// not a forced full text repaint, check text difference
+		if (!bFullRepaint) bFullRepaint = (GetBufferDifference() > 15);
+
+		// repaint text layer
+ 		if (bFullRepaint)
+ 		{
+			RepaintText(m_dcText);
+		}
+		else
+		{
+			RepaintTextChanges(m_dcText);
+		}
+	}
+
 	BitBltOffscreen();
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+void ConsoleView::MainframeMoving()
+{
+	// next OnPaint will do a full repaint
+	if (m_tabData->imageData.bRelative) m_bNeedFullRepaint = true;
 }
 
 //////////////////////////////////////////////////////////////////////////////
@@ -1099,7 +1129,7 @@ void ConsoleView::SetActive(bool bActive)
 	m_bActive = bActive;
 	if (!m_bActive) return;
 
-	RepaintView();
+	Repaint(true);
 	UpdateTitle();
 }
 
@@ -1620,40 +1650,81 @@ void ConsoleView::UpdateTitle()
 
 
 /////////////////////////////////////////////////////////////////////////////
-
+/*
 void ConsoleView::Repaint()
 {
+//	RepaintText(m_dcText);
+//	RepaintTextChanges(m_dcText);
+
 	// repaint text layer
  	if (GetBufferDifference() > 15)
  	{
-		RepaintText();
+		RepaintText(m_dcText);
 	}
 	else
 	{
-		RepaintTextChanges();
+		RepaintTextChanges(m_dcText);
 	}
 
 	BitBltOffscreen();
 }
-
+*/
 /////////////////////////////////////////////////////////////////////////////
 
 
 /////////////////////////////////////////////////////////////////////////////
 
-void ConsoleView::RepaintText()
+void ConsoleView::RepaintText(CDC& dc)
 {
 	SIZE	bitmapSize;
 	CRect	bitmapRect;
 
-	m_bmpText.GetSize(bitmapSize);
+	dc.GetCurrentBitmap().GetSize(bitmapSize);
 	bitmapRect.left		= 0;
 	bitmapRect.top		= 0;
 	bitmapRect.right	= bitmapSize.cx;
 	bitmapRect.bottom	= bitmapSize.cy;
 
-	m_dcText.FillRect(&bitmapRect, m_backgroundBrush);
-	
+	if (m_tabData->backgroundImageType == bktypeNone)
+	{
+		dc.FillRect(&bitmapRect, m_backgroundBrush);
+	}
+	else
+	{
+		CRect	rectWindow;
+		GetClientRect(&rectWindow);
+
+		g_imageHandler->UpdateImageBitmap(dc, rectWindow, m_background);
+
+		if (m_tabData->imageData.bRelative)
+		{
+			CPoint	pointClientScreen(0, 0);
+			ClientToScreen(&pointClientScreen);
+
+			dc.BitBlt(
+				rectWindow.left, 
+				rectWindow.top, 
+				rectWindow.right, 
+				rectWindow.bottom, 
+				m_background->dcImage, 
+				rectWindow.left + pointClientScreen.x - ::GetSystemMetrics(SM_XVIRTUALSCREEN), 
+				rectWindow.top + pointClientScreen.y - ::GetSystemMetrics(SM_YVIRTUALSCREEN), 
+				SRCCOPY);
+		}
+		else
+		{
+			dc.BitBlt(
+				bitmapRect.left, 
+				bitmapRect.top, 
+				bitmapRect.right, 
+				bitmapRect.bottom, 
+				m_background->dcImage, 
+				bitmapRect.left, 
+				bitmapRect.top, 
+				SRCCOPY);
+		}
+	}
+
 	StylesSettings&					stylesSettings	= g_settingsHandler->GetAppearanceSettings().stylesSettings;
 	SharedMemory<ConsoleParams>&	consoleParams	= m_consoleHandler.GetConsoleParams();
 	MutexLock						bufferLock(m_bufferMutex);
@@ -1699,11 +1770,11 @@ void ConsoleView::RepaintText()
 			crBkColor	= m_consoleSettings.consoleColors[attrBG];
 		}
 
-		m_dcText.SetBkMode(nBkMode);
-		m_dcText.SetBkColor(crBkColor);
+		dc.SetBkMode(nBkMode);
+		dc.SetBkColor(crBkColor);
 
 		crTxtColor		= m_appearanceSettings.fontSettings.bUseColor ? m_appearanceSettings.fontSettings.crFontColor : m_consoleSettings.consoleColors[m_screenBuffer[dwOffset].charInfo.Attributes & 0xF];
-		m_dcText.SetTextColor(crTxtColor);
+		dc.SetTextColor(crTxtColor);
 
 		strText		= m_screenBuffer[dwOffset].charInfo.Char.UnicodeChar;
 		m_screenBuffer[dwOffset].changed = false;
@@ -1752,18 +1823,18 @@ void ConsoleView::RepaintText()
 
 			if (bTextOut)
 			{
-//				m_dcText.TextOut(dwX, dwY, strText.c_str(), static_cast<int>(strText.length()));
+//				dc.TextOut(dwX, dwY, strText.c_str(), static_cast<int>(strText.length()));
 
 //				CRect textOutRect(dwX, dwY, dwX+m_nCharWidth*strText.length(), dwY+m_nCharHeight);
 				CRect textOutRect(dwX, dwY, dwX+m_nCharWidth*nCharWidths, dwY+m_nCharHeight);
 
-				m_dcText.ExtTextOut(dwX, dwY, 0, &textOutRect, strText.c_str(), static_cast<int>(strText.length()), NULL);
+				dc.ExtTextOut(dwX, dwY, 0, &textOutRect, strText.c_str(), static_cast<int>(strText.length()), NULL);
 //				dwX += static_cast<int>(strText.length() * m_nCharWidth);
 				dwX += static_cast<int>(nCharWidths * m_nCharWidth);
 
-				m_dcText.SetBkMode(nBkMode);
-				m_dcText.SetBkColor(crBkColor);
-				m_dcText.SetTextColor(crTxtColor);
+				dc.SetBkMode(nBkMode);
+				dc.SetBkColor(crBkColor);
+				dc.SetTextColor(crTxtColor);
 
 				strText		= m_screenBuffer[dwOffset].charInfo.Char.UnicodeChar;
 				m_screenBuffer[dwOffset].changed = false;
@@ -1780,10 +1851,10 @@ void ConsoleView::RepaintText()
 
 		if (strText.length() > 0)
 		{
-//			m_dcText.TextOut(dwX, dwY, strText.c_str(), static_cast<int>(strText.length()));
+//			dc.TextOut(dwX, dwY, strText.c_str(), static_cast<int>(strText.length()));
 //			CRect textOutRect(dwX, dwY, dwX+m_nCharWidth*strText.length(), dwY+m_nCharHeight);
 			CRect textOutRect(dwX, dwY, dwX+m_nCharWidth*nCharWidths, dwY+m_nCharHeight);
-			m_dcText.ExtTextOut(dwX, dwY, 0, &textOutRect, strText.c_str(), static_cast<int>(strText.length()), NULL);
+			dc.ExtTextOut(dwX, dwY, 0, &textOutRect, strText.c_str(), static_cast<int>(strText.length()), NULL);
 		}
 	}
 }
@@ -1793,12 +1864,8 @@ void ConsoleView::RepaintText()
 
 /////////////////////////////////////////////////////////////////////////////
 
-void ConsoleView::RepaintTextChanges()
+void ConsoleView::RepaintTextChanges(CDC& dc)
 {
-	SIZE	bitmapSize;
-
-	m_bmpText.GetSize(bitmapSize);
-
 	StylesSettings& stylesSettings = g_settingsHandler->GetAppearanceSettings().stylesSettings;
 
 	DWORD	dwX			= stylesSettings.dwInsideBorder;
@@ -1808,6 +1875,11 @@ void ConsoleView::RepaintTextChanges()
 	WORD	attrBG;
 
 	MutexLock bufferLock(m_bufferMutex);
+
+	CRect	rectWindow;
+	GetClientRect(&rectWindow);
+
+	if (m_tabData->backgroundImageType != bktypeNone) g_imageHandler->UpdateImageBitmap(dc, rectWindow, m_background);
 
 	for (DWORD i = 0; i < m_consoleHandler.GetConsoleParams()->dwRows; ++i)
 	{
@@ -1829,28 +1901,67 @@ void ConsoleView::RepaintTextChanges()
 				// we have to erase two spaces for double-width characters
 				rect.right	= (m_screenBuffer[dwOffset].charInfo.Attributes & COMMON_LVB_LEADING_BYTE) ? dwX + 2*m_nCharWidth : dwX + m_nCharWidth;
 				
-				m_dcText.FillRect(&rect, m_backgroundBrush);
+				if (m_tabData->backgroundImageType == bktypeNone)
+				{
+					dc.FillRect(&rect, m_backgroundBrush);
+				}
+				else
+				{
+					if (m_tabData->imageData.bRelative)
+					{
+						CPoint	pointClientScreen(0, 0);
+						ClientToScreen(&pointClientScreen);
+
+						dc.BitBlt(
+							rect.left, 
+							rect.top, 
+							rect.Width(), 
+							rect.Height(), 
+							m_background->dcImage, 
+							rect.left + pointClientScreen.x - ::GetSystemMetrics(SM_XVIRTUALSCREEN), 
+							rect.top + pointClientScreen.y - ::GetSystemMetrics(SM_YVIRTUALSCREEN), 
+							SRCCOPY);
+
+						// RepaintTextChanges is never called for relative backgrounds
+//						assert(false);
+						// TODO: blit relative image
+//						dc.FillRect(&rect, m_backgroundBrush);
+					}
+					else
+					{
+						dc.BitBlt(
+							rect.left, 
+							rect.top, 
+							rect.Width(),
+							rect.Height(),
+							m_background->dcImage, 
+							rect.left, 
+							rect.top, 
+							SRCCOPY);
+//						dc.FillRect(&rect, bb);
+					}
+				}
 
 				attrBG = (m_screenBuffer[dwOffset].charInfo.Attributes & 0xFF) >> 4;
 
 				// here we decide how to paint text over the background
 				if (m_consoleSettings.consoleColors[attrBG] == RGB(0, 0, 0))
 				{
-					m_dcText.SetBkMode(TRANSPARENT);
+					dc.SetBkMode(TRANSPARENT);
 				}
 				else
 				{
-					m_dcText.SetBkMode(OPAQUE);
-					m_dcText.SetBkColor(m_consoleSettings.consoleColors[attrBG]);
+					dc.SetBkMode(OPAQUE);
+					dc.SetBkColor(m_consoleSettings.consoleColors[attrBG]);
 				}
 				
-				m_dcText.SetBkColor(m_consoleSettings.consoleColors[attrBG]);
-				m_dcText.SetTextColor(m_appearanceSettings.fontSettings.bUseColor ? m_appearanceSettings.fontSettings.crFontColor : m_consoleSettings.consoleColors[m_screenBuffer[dwOffset].charInfo.Attributes & 0xF]);
-//				m_dcText.TextOut(dwX, dwY, &(m_screenBuffer[dwOffset].Char.UnicodeChar), 1);
+				dc.SetBkColor(m_consoleSettings.consoleColors[attrBG]);
+				dc.SetTextColor(m_appearanceSettings.fontSettings.bUseColor ? m_appearanceSettings.fontSettings.crFontColor : m_consoleSettings.consoleColors[m_screenBuffer[dwOffset].charInfo.Attributes & 0xF]);
+//				dc.TextOut(dwX, dwY, &(m_screenBuffer[dwOffset].charInfo.Char.UnicodeChar), 1);
 
 //				CRect textOutRect(dwX, dwY, dwX+m_nCharWidth, dwY+m_nCharHeight);
 
-				m_dcText.ExtTextOut(dwX, dwY, ETO_CLIPPED, &rect, &(m_screenBuffer[dwOffset].charInfo.Char.UnicodeChar), 1, NULL);
+				dc.ExtTextOut(dwX, dwY, ETO_CLIPPED, &rect, &(m_screenBuffer[dwOffset].charInfo.Char.UnicodeChar), 1, NULL);
 			}
 		}
 	}
@@ -1885,7 +1996,7 @@ void ConsoleView::BitBltOffscreen(bool bOnlyCursor /*= false*/)
 		GetClientRect(&rectBlit);
 	}
 
-	if ((m_tabData->backgroundImageType == bktypeNone) || !m_tabData->imageData.bRelative)
+//	if ((m_tabData->backgroundImageType == bktypeNone) || !m_tabData->imageData.bRelative)
 	{
 		// we don't do this for relative backgrounds here
 		UpdateOffscreen(rectBlit);
@@ -1901,8 +2012,11 @@ void ConsoleView::BitBltOffscreen(bool bOnlyCursor /*= false*/)
 
 void ConsoleView::UpdateOffscreen(const CRect& rectBlit)
 {
+/*
 	CRect	rectWindow;
 	GetClientRect(&rectWindow);
+
+	g_imageHandler->UpdateImageBitmap(m_dcOffscreen, rectWindow, m_background);
 
 	if (m_tabData->backgroundImageType != bktypeNone)
 	{
@@ -2012,6 +2126,17 @@ void ConsoleView::UpdateOffscreen(const CRect& rectBlit)
 							SRCCOPY);
 		}
 	}
+*/
+
+	m_dcOffscreen.BitBlt(
+					rectBlit.left, 
+					rectBlit.top, 
+					rectBlit.right, 
+					rectBlit.bottom, 
+					m_dcText, 
+					rectBlit.left, 
+					rectBlit.top, 
+					SRCCOPY);
 
 	// blit cursor
 	if (m_consoleHandler.GetCursorInfo()->bVisible && (m_cursor.get() != NULL))

Console/ConsoleView.h
@@ -110,7 +110,8 @@ class ConsoleView
 		void SetAppActiveStatus(bool bAppActive);
 
 		void RecreateOffscreenBuffers();
-		void RepaintView();
+		void Repaint(bool bFullRepaint);
+		void MainframeMoving();
 
 		void SetResizing(bool bResizing);
 		void SetActive(bool bActive);
@@ -141,10 +142,9 @@ class ConsoleView
 		DWORD GetBufferDifference();
 
 		void UpdateTitle();
-		void Repaint();
 
-		void RepaintText();
-		void RepaintTextChanges();
+		void RepaintText(CDC& dc);
+		void RepaintTextChanges(CDC& dc);
 
 		void BitBltOffscreen(bool bOnlyCursor = false);
 		void UpdateOffscreen(const CRect& rectBlit);
@@ -169,6 +169,7 @@ class ConsoleView
 		bool	m_bResizing;
 		bool	m_bAppActive;
 		bool	m_bActive;
+		bool	m_bNeedFullRepaint;
 		bool	m_bUseTextAlphaBlend;
 		bool	m_bConsoleWindowVisible;
 

Console/stdafx.h
@@ -107,7 +107,7 @@ using namespace boost::multi_index;
 
 #define	VERSION_MAJOR	2
 #define	VERSION_MINOR	0
-#define	VERSION_BUILD	142
+#define	VERSION_BUILD	144
 
 //////////////////////////////////////////////////////////////////////////////
 

ConsoleHook/ConsoleHook.rc
@@ -53,8 +53,8 @@ END
 //
 
 VS_VERSION_INFO VERSIONINFO
- FILEVERSION 2,0,0,140
- PRODUCTVERSION 2,0,0,140
+ FILEVERSION 2,0,0,144
+ PRODUCTVERSION 2,0,0,144
  FILEFLAGSMASK 0x3fL
 #ifdef _DEBUG
  FILEFLAGS 0x1L
@@ -72,13 +72,13 @@ BEGIN
             VALUE "Comments", "Tabbed cool console window (THIS IS NOT A SHELL :-)"
             VALUE "CompanyName", "Ingenuity Unlimited Ltd."
             VALUE "FileDescription", "Console process hook module"
-            VALUE "FileVersion", "2.00, Build 140 2008.08.21"
+            VALUE "FileVersion", "2.00, Build 144 2009.03.23"
             VALUE "InternalName", "Console"
-            VALUE "LegalCopyright", "Copyright © 2001-2008 Marko Bozikovic"
-            VALUE "LegalTrademarks", "Copyright © 2001-2008 Marko Bozikovic"
+            VALUE "LegalCopyright", "Copyright © 2001-2009 Marko Bozikovic"
+            VALUE "LegalTrademarks", "Copyright © 2001-2009 Marko Bozikovic"
             VALUE "OriginalFilename", "Console.exe"
             VALUE "ProductName", "Console"
-            VALUE "ProductVersion", "2.00.00.140"
+            VALUE "ProductVersion", "2.00.00.144"
         END
     END
     BLOCK "VarFileInfo"

ConsoleHook/ConsoleHook.vcproj
@@ -48,7 +48,7 @@
 				RuntimeLibrary="3"
 				UsePrecompiledHeader="2"
 				WarningLevel="4"
-				Detect64BitPortabilityProblems="true"
+				Detect64BitPortabilityProblems="false"
 				DebugInformationFormat="4"
 			/>
 			<Tool
@@ -124,7 +124,7 @@
 				RuntimeLibrary="2"
 				UsePrecompiledHeader="2"
 				WarningLevel="4"
-				Detect64BitPortabilityProblems="true"
+				Detect64BitPortabilityProblems="false"
 				DebugInformationFormat="3"
 			/>
 			<Tool

commit bf4bdc8f4b7c6c9e14620c9346cde51d5adea5fe

Improve handling of WM_SETTINGCHANGE, including forced full repaint
The change is inspired by Patch 2781047 "BugFix for 1632808/2002160 (b144)"
by GrayNM:
Solution, used to detect wallpaper changes, doesn`t work for utilities
like "Wallpaper Master" etc.

Moreover background repainting doesn`t occur even if change was detected

Notes by Kirill:
- 1632808 was closed as fixed long time ago, but was broken by b8176836;
- FR 2002160 (aka 2924201) is indeed would be implemented by the patch;

However, the doc on WM_SETTINGCHANGE makes no guarantees about wParam, thus
this is more conservative, but probably more costly fix.

Console/MainFrame.cpp
@@ -783,14 +783,21 @@ LRESULT MainFrame::OnSettingChange(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM lPar
 
 	wstring strArea(reinterpret_cast<wchar_t*>(lParam));
 
-	if (strArea == L"Windows")
-	{
-		g_imageHandler->ReloadDesktopImages();
-		m_activeView->Invalidate();
-	}
-	else if (strArea == L"Environment")
+	// according to WM_SETTINGCHANGE doc:
+	// to change environment, lParam should be "Environment"
+	if (strArea == L"Environment")
 	{
 		ConsoleHandler::UpdateEnvironmentBlock();
+	}
+	else
+	{
+		// otherwise, we don't know what has changed
+		// technically, we can skip reloading for "Policy" and "intl", but
+		// hopefully they don't happen often, so reload everything
+		g_imageHandler->ReloadDesktopImages();
+
+		// can't use Invalidate because full repaint is in order
+		m_activeView->Repaint(true);
 	}
 
 	return 0;